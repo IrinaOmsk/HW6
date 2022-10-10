15c5cb6900726df65e5f978167f7b19cc7ae42b5
Irina <I.M.ZUBKOVA@GMAIL.COM>
2022-10-10 19:46:58 +0600

    add files

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int[] array = new int[InputIntNumber()];
+EnterArray(array, 0);
+PrintArray(array);
+CountOfNumBiggerThenZero(array);
+
+int InputIntNumber()
+{
+        Console.Write("Ведите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+}
+
+void EnterArray(int[] array, int i)
+{
+
+    if (i < array.Length)
+    {
+        Console.Write($"Введите {i+1} эл-т массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        i++;
+        EnterArray(array, i);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+}
+
+void CountOfNumBiggerThenZero(int[] array)
+{
+    Random rand = new Random();
+    int length = array.Length;
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Пользователь ввел {count} чисел больше 0.");
+}
---
+++ Zadacha_43/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых,
+//   заданных уравнениями 
+//   y = k1 * x + b1,
+//    y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[,] array = new Double[2, 2];
+FillArray(array);
+CrossPoint(array);
+
+void FillArray(double[,] matrixArray)
+{
+    Console.Write("Задай коэффициент k1 = ");
+    matrixArray[0, 0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Задай коэффициент b1 = ");
+    matrixArray[0, 1] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Задай коэффициент k2 = ");
+    matrixArray[1, 0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Задай коэффициент b2 = ");
+    matrixArray[1, 1] = Convert.ToInt32(Console.ReadLine());
+}
+
+void CrossPoint(double[,] matrixArray)
+{  
+    double x = Math.Round((matrixArray[1, 1] - matrixArray[0, 1]) / (matrixArray[0, 0] - matrixArray[1, 0]), 2);
+    double y = Math.Round(matrixArray[0, 0] * x + matrixArray[0, 1], 2);
+
+    Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y})");
+}
+
+
